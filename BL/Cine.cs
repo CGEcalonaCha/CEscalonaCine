f7878e0245cce61c0efdaa12f93eb7d490916186
Cecilia Gabriela Escalona Chavarria <cescalonacha@gmail.com>
2023-05-30 19:17:23 -0600

    Add project files.

---
+++ BL/Cine.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Cine
+    {
+        public static ML.Result Add(ML.Cine cine)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CescalonaCineContext contex = new DL.CescalonaCineContext())
+                {
+                    int RowsAfected = contex.Database.ExecuteSqlRaw($"CineAdd '{cine.Nombre}', '{cine.Direccion}', {cine.Zona.IdZona}, {cine.Venta}");
+
+                    if (RowsAfected > 0)
+                    {
+                        result.Correct = true; ;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al ingresar el cine";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+
+
+        public static ML.Result Update(ML.Cine cine)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CescalonaCineContext contex = new DL.CescalonaCineContext())
+                {
+                    int RowsAfected = contex.Database.ExecuteSqlRaw($"CineUpdate {cine.IdCine}, '{cine.Nombre}', '{cine.Direccion}', {cine.Zona.IdZona}, {cine.Venta}");
+
+                    if (RowsAfected > 0)
+                    {
+                        result.Correct = true; ;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al Actualizar el cine";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+
+        public static ML.Result Delete(int idCine)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.CescalonaCineContext contex = new DL.CescalonaCineContext())
+                {
+                    int RowsAfected = contex.Database.ExecuteSqlRaw($"CineDelete {idCine}");
+
+                    if (RowsAfected > 0)
+                    {
+                        result.Correct = true; ;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrio un error al Elimar el cine";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+            }
+
+            return result;
+        }
+
+        //public static ML.Result GetAll()
+        //{
+        //    ML.Result result = new ML.Result();
+        //    try
+        //    {
+        //        using (DL.CescalonaCineContext contex = new DL.CescalonaCineContext())
+        //        {
+        //            var RowsAfected = contex.Cines.FromSqlRaw("CineGetAll").ToList();
+
+        //            result.Objects = new List<object>();
+
+        //            if (contex != null)
+        //            {
+        //                foreach (var obj in RowsAfected)
+        //                {
+        //                    ML.Cine cine = new ML.Cine();
+        //                    cine.IdCine = obj.IdCine;
+        //                    cine.Nombre = obj.Nombre;
+        //                    cine.Direccion = obj.Direccion;
+        //                    cine.Venta = obj.Venta
+        //                    cine.Zona = new ML.Zona();
+        //                    cine.Zona.IdZona = (int)obj.IdZona;
+        //                    cine.Zona.Nombre = obj.Zona;
+
+        //                    result.Objects.Add(cine);
+        //                }
+        //                result.Correct = true;
+        //            }
+        //            else
+        //            {
+        //                result.Correct = false;
+        //                result.ErrorMessage = "No se encontraron registros.";
+        //            }
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result.Correct = false;
+        //        result.ErrorMessage = ex.Message;
+        //    }
+        //    return result;
+        //}
+
+        //public static ML.Result GetById(int idCine)
+        //{
+        //    ML.Result result = new ML.Result();
+        //    try
+        //    {
+        //        using (DL.CescalonaCineContext contex = new DL.CescalonaCineContext())
+        //        {
+        //            var RowsAfected = contex.Cines.FromSqlRaw($"CineGetById {idCine}").AsEnumerable().FirstOrDefault();
+
+        //            result.Object = new object();
+
+        //            if (RowsAfected != null)
+        //            {
+        //                ML.Cine cine = new ML.Cine();
+        //                cine.IdCine = RowsAfected.IdCine;
+        //                cine.Nombre = RowsAfected.Nombre;
+        //                cine.Direccion = RowsAfected.Direccion;
+        //                cine.Venta = (int)RowsAfected.Ventas;
+        //                cine.Zona = new ML.Zona();
+        //                cine.Zona.IdZona = (int)RowsAfected.IdZona;
+        //                cine.Zona.Nombre = RowsAfected.Zona;
+        //                result.Object = cine;
+
+        //                result.Correct = true;
+        //            }
+        //            else
+        //            {
+        //                result.Correct = false;
+        //                result.ErrorMessage = "Ocurrió un error al obtener el registros en la tabla Cine";
+        //            }
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        result.Correct = false;
+        //        result.ErrorMessage = ex.Message;
+        //    }
+        //    return result;
+        //}
+
+
+    }
+}
---
+++ DL/CescalonaCineContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DL;
+
+public partial class CescalonaCineContext : DbContext
+{
+    public CescalonaCineContext()
+    {
+    }
+
+    public CescalonaCineContext(DbContextOptions<CescalonaCineContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cine> Cines { get; set; }
+
+    public virtual DbSet<Zona> Zonas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.; Database= CEscalonaCine; TrustServerCertificate=True; User ID=sa; Password=pass@word1;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cine>(entity =>
+        {
+            entity.HasKey(e => e.IdCine).HasName("PK__Cine__394B724B056E2FEA");
+
+            entity.ToTable("Cine");
+
+            entity.Property(e => e.Direccion)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Venta)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdZonaNavigation).WithMany(p => p.Cines)
+                .HasForeignKey(d => d.IdZona)
+                .HasConstraintName("FK__Cine__IdZona__1273C1CD");
+        });
+
+        modelBuilder.Entity<Zona>(entity =>
+        {
+            entity.HasKey(e => e.IdZona).HasName("PK__Zona__F631C12D7DAD67D0");
+
+            entity.ToTable("Zona");
+
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
